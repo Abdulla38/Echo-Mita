6c22b39b5356f1315618c11b2e59c63ecb42daf0
Abdulla <abdullakurmanbaev38@gmail.com>
2026-06-12 07:52:16 +0600

    feat: add json parser and flags flags for compilation

--- nob.c
+++ nob.c
@@ -10,10 +10,22 @@ int main(int argc, char **argv) {
   nob_cmd_append(&cmd, SRC_FOLDER "main.c");
   nob_cmd_append(&cmd, SRC_FOLDER "audio/audio_capture.c");
   nob_cmd_append(&cmd, SRC_FOLDER "audio/audio_playback.c");
+  nob_cmd_append(&cmd, SRC_FOLDER "parser-json/parser.c");
   nob_cmd_append(&cmd, SRC_FOLDER "speech/speech_recognition.c");
   nob_cmd_append(&cmd, "-o", BUILD_FOLDER "Mita");
   nob_cmd_append(&cmd, "-Wall", "-Wextra", "-pedantic", "-lm", "-ldl",
-                 "-lpthread", "-lvosk");
+                 "-lpthread", "-lvosk", "-ljson-c");
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "--debug") == 0) {
+      nob_cmd_append(&cmd, "-ggdb");
+      nob_cmd_append(&cmd, "-fsanitize=address");
+    }
+    if (strcmp(argv[i], "--release") == 0) {
+      nob_cmd_append(&cmd, "-O2");
+      nob_cmd_append(&cmd, "-march=native");
+    }
+  }
+  nob_cmd_append(&cmd, "-lraylib");
   if (!nob_cmd_run(&cmd))
     return 1;
   return 0;
